7375a621794f19a45dfdff319f81bd430d68520d
Jose Luis De Miguel Pérez <jl.demiguel@alumnos.upm.es>
2022-07-30 20:02:21 +0200

    Add acciones msgconf

--- views/auth/msgconf.php
+++ views/auth/msgconf.php
@@ -5,5 +5,11 @@
     <div class="contenedor-sm">
         <p class="descripcion-pagina">Hemos enviado las instrucciones para confirmar tu cuenta a tu email</p>
 
+        <div class="acciones">
+            <a href="/">¿Ya tienes una cuenta? Inicia sesión</a>
+            <a href="/crear">Aun no tienes una cuenta? Obten una</a>
+        </div>
     </div>
+
+
 </div>
